32a340c025facf57cc3f28326f5c21ac7cdc0fe9
Попович Олег Игоревич <popovich.oi@esphere.ru>
2021-08-10 22:26:54 +0300

    multi-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import * as yup from 'yup';
+import InputField from './InputField';
 import './App.css';
+import MultiStepForm, { FormStep } from './MultiStepForm';
+
+const personValidationSchema = yup.object({
+  name: yup.string().required('Name is required'),
+  email: yup.string().required('Email is required')
+})
+
+const addressValidationSchema = yup.object({
+  street: yup.string().required('Street is required'),
+  country: yup.string().required('Country is required')
+})
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <MultiStepForm
+          initialValues={{
+            name: '',
+            email: '',
+            street: '',
+            country: ''
+          }}
+          onSubmit={(values) => {
+            console.log(JSON.stringify(values, null, 3));
+          }}
         >
-          Learn React
-        </a>
+          <FormStep
+            stepName="Person"
+            onSubmit={() => console.log("STEP 1")}
+            validationSchema={personValidationSchema}
+          >
+            <InputField name="name" label="Name" />
+            <InputField name="email" label="Email" />
+          </FormStep>
+          <FormStep
+            stepName="Address"
+            onSubmit={() => console.log("STEP 2")}
+            validationSchema={addressValidationSchema}
+          >
+            <InputField name="street" label="Street" />
+            <InputField name="country" label="Coutry" />
+          </FormStep>
+        </MultiStepForm>
       </header>
     </div>
   );
